fc55fcae7df2b62f04d9b3bbbcf1738e2def0eec
st96122 <st96122@gmail.com>
2018-05-28 11:28:22 +0800

    Add files via upload

--- reccsv.py
+++ reccsv.py
@@ -1,14 +1,24 @@
 import csv
 import cv2
 import numpy as np
+import time
 import thread
-def f0x25(j,frame):
+def f0x25(j,frame,kj):
+    global tt2
+    if (tt2 == 0 or tt2 != kj):
 
-
-    v=int(j[3][4:6],16)
-    v=bin(v)
-    v=v[2:len(v)].zfill(12)
-    print(int(v,2))
+        v=int(j[3][3:6],16)
+        v=bin(v)
+        v=v[2:len(v)].zfill(12)
+        v=str(v)
+        if(v[0]=='0'):
+            v=int(v,2)
+            cv2.putText(frame, str(v), (550, 250), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 6)
+        else:
+            nk=nott(v,len(v))
+            nk=int(nk,2)
+            cv2.putText(frame, '-'+str(nk), (550, 250), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 6)
+        tt2=kj
 def f0x230(j,frame):
 
     v = (int(j[3], 16))
@@ -25,6 +35,7 @@ def f0x230(j,frame):
         cv2.putText(frame, 'brake_L', (50, 200), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 6)
     else:
         print(j[3] + ' no_brake')
+        cv2.putText(frame, 'no_brake', (50, 200), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 6)
 
 def f0x5b6(j,frame):
 
@@ -59,6 +70,12 @@ def f0x120(j,frame):
         '20': lambda:pr('p',j[3][12:14]),
         '22': lambda:pr('n',j[3][12:14])
     }.get(j[3][12:14], lambda:pr('',j[3][12:14]))()
+def f0xb4(j,frame,kj):
+    v=int(j[3][12:16],16)*0.01
+    global tt
+    if(tt == 0 or tt!=kj):
+        cv2.putText(frame, str(v), (0, 400), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 6)
+        tt=kj
 def pr(k,j):
     print(j+'  '+k)
     cv2.putText(frame, k, (0, 500), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 6)
@@ -80,7 +97,16 @@ def primg(kk):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cap.release()
         cv2.destroyAllWindows()
-
+def nott(v,le):
+    k=''
+    for i in range(0,le):
+        if v[i]=='0':
+            k=k+'1'
+        else:
+            k=k+'0'
+    return k
+tt=0
+tt2=0
 cap=cv2.VideoCapture('0524.avi')
 ret,frame=cap.read()
 with open('canbus_output2.csv','r') as csvf:
@@ -108,15 +134,17 @@ with open('canbus_output2.csv','r') as csvf:
                         break
                 '''
             if(kji!=kj):
+                time.sleep(0.03)
                 prvideo(frame)
                 ret, frame = cap.read()
             {
 
-                '0x25': lambda: f0x25(j,frame),
+                '0x25': lambda: f0x25(j,frame,kj),
                 '0x230': lambda: f0x230(j,frame),
                 '0x5b6': lambda: f0x5b6(j,frame),
                 '0x57f': lambda: f0x57f(j,frame),
-                '0x120': lambda: f0x120(j,frame)
+                '0x120': lambda: f0x120(j,frame),
+                '0xb4': lambda: f0xb4(j,frame,kj)
             }.get(j[2], lambda: None)()
 
                 #primg(lk[kj][1])
